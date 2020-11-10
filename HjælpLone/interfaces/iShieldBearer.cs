2840fdf1b577cc40b9ca0e8be903e56d166f8655
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2020-11-10 14:32:59 +0100

    Initial commit

---
+++ HjælpLone/interfaces/iShieldBearer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HjælpLone
+{
+    interface iShieldBearer : iCharacter
+    {
+        void RaiseShield();
+        void ShieldGlare();
+    }
+}
